b95b5b72a8524fa237ca541fd6d02a9a60e66a1e
efrainpardoqmcorp <efrain.pardo@quantummaterialscorp.com>
2021-05-05 19:13:09 -0500

    agregar funcionalidad de actualizar tabla y estilos en la página de contactos

--- frontend/js/access.js
+++ frontend/js/access.js
@@ -1,7 +1,6 @@
 // Validación de las páginas que son publicas y que no necesitan token
 const public = [
-    '/login.html',
-    '/register.html'
+    '/login.html'
 ]
 if(!public.includes(window.location.pathname) && !localStorage.getItem('sesionToken')){
     window.location.href = '/login.html';
