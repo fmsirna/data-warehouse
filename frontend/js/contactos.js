bc385dc21fd6ad86efafbc83ec91df2bb3ec08c8
efrainpardoqmcorp <efrain.pardo@quantummaterialscorp.com>
2021-05-05 19:57:53 -0500

    add new features to data table

--- frontend/js/contactos.js
+++ frontend/js/contactos.js
@@ -1,49 +1,92 @@
-var data = [
-  {
-    'id': 0,
-    'name': 'Item 0',
-    'price': '$0'
+function operateFormatter(value, row, index) {
+  return [
+    '<a class="like" href="javascript:void(0)" title="Like">',
+    '<i class="fa fa-heart"></i>',
+    '</a>  ',
+    '<a class="remove" href="javascript:void(0)" title="Remove">',
+    '<i class="fa fa-trash"></i>',
+    '</a>'
+  ].join('')
+}
+
+window.operateEvents = {
+  'click .like': function (e, value, row, index) {
+    alert('You click like action, row: ' + JSON.stringify(row))
   },
-  {
-    'id': 1,
-    'name': 'Item 1',
-    'price': '$1'
-  },
-  {
-    'id': 2,
-    'name': 'Item 2',
-    'price': '$2'
-  },
-  {
-    'id': 3,
-    'name': 'Item 3',
-    'price': '$3'
-  },
-  {
-    'id': 4,
-    'name': 'Item 4',
-    'price': '$4'
-  },
-  {
-    'id': 5,
-    'name': 'Item 5',
-    'price': '$2005'
-  },
-  {
-    'id': 6,
-    'name': 'Item 6',
-    'price': '$6'
-  },
-  {
-    'id': 7,
-    'name': 'Item 7',
-    'price': '$7'
+  'click .remove': function (e, value, row, index) {
+    $table.bootstrapTable('remove', {
+      field: 'id',
+      values: [row.id]
+    })
   }
-]
+}
 
 // Inicializar tabla
+var data = [
+  {
+    'id': 1,
+    'contact': 'Contacto 1',
+    'region': 'Region 1',
+    'company': 'Compañía 1',
+    'position': 'Cargo 1',
+    'channel': 'Canal 1'
+  }
+]
 let $table = $('#table');
-$table.bootstrapTable({data: data});
+$table.bootstrapTable({
+  data: data,
+  locale: 'es-AR',
+  columns: [
+    {
+      field: 'selection',
+      checkbox: true,
+      align: 'center',
+      valign: 'middle'
+    }, {
+      title: 'Contacto',
+      field: 'contact',
+      align: 'center',
+      valign: 'middle',
+      sortable: true
+    }, {
+      title: 'País/Region',
+      field: 'region',
+      align: 'center',
+      valign: 'middle',
+      sortable: true
+    }, {
+      title: 'Compañía',
+      field: 'company',
+      align: 'center',
+      valign: 'middle',
+      sortable: true
+    }, {
+      title: 'Cargo',
+      field: 'position',
+      align: 'center',
+      valign: 'middle',
+      sortable: true
+    }, {
+      title: 'Canal preferido',
+      field: 'channel',
+      align: 'center',
+      valign: 'middle'
+    }, {
+      title: 'Interés',
+      field: 'interet',
+      align: 'center',
+      valign: 'middle',
+      sortable: true
+    }, {
+      field: 'operate',
+      title: 'Acciones',
+      align: 'center',
+      clickToSelect: false,
+      events: window.operateEvents,
+      formatter: operateFormatter
+    }
+  ]
+});
 
 // Función para actualizar los datos
 const updateTable = data => {
